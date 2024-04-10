b8cfad370c1ce6d7d092a215a3463b766eb2e599
viktorcsimma <csimmaviktor03@gmail.com>
2024-04-10 20:16:56 +0200

    Adding progress indicator to PreciseOutputWindow

--- src/View/PreciseOutputWindow.cpp
+++ src/View/PreciseOutputWindow.cpp
@@ -2,6 +2,8 @@
 #include "View/ui_PreciseOutputWindow.h"
 #include "PreciseOutputWindow.moc"
 
+#include <QMovie>
+
 PreciseOutputWindow::PreciseOutputWindow(PreciseOutputViewModel& viewModel, QWidget* parent)
     : QDialog(parent)
     , ui(new Ui::PreciseOutputWindow)  // don't forget! this caused me a lot of segfaults and headaches
@@ -13,6 +15,18 @@ PreciseOutputWindow::PreciseOutputWindow(PreciseOutputViewModel& viewModel, QWid
     ui->textEdit->setText(QString::fromStdString(viewModel.getResult()));
 
     ui->spinBox->setValue(viewModel.getPrecision());
+
+    // we set the image for busyIndicatorLabel but hide it
+    QSizePolicy sizePolicy = ui->busyIndicatorLabel->sizePolicy(); // for some reason, this cannot be done in a single step
+    sizePolicy.setRetainSizeWhenHidden(true); // this way, its place won't be "eaten up" by others if hidden
+    ui->busyIndicatorLabel->setSizePolicy(sizePolicy);
+    ui->busyIndicatorLabel->setVisible(false);
+    //the label itself will be the parent of the movie;
+    //this way, we don't have to free it by ourselves
+    QMovie* movie = new QMovie("assets/busy_indicator.gif", QByteArray(), ui->busyIndicatorLabel);
+    movie->setScaledSize(QSize(26, 26));
+    movie->start();
+    ui->busyIndicatorLabel->setMovie(movie);
 
     connect(ui->evaluateButton, &QPushButton::clicked, this, &PreciseOutputWindow::evaluateButtonClicked);
     // we do this in order to ensure that onEvaluationFinished runs in the main thread
@@ -41,13 +55,14 @@ void PreciseOutputWindow::onEvaluationFinished()
     ui->textEdit->setText(QString::fromStdString(viewModel.getResult()));
     ui->evaluateButton->setText("Evaluate");
     ui->evaluateButton->setEnabled(true);
+    ui->busyIndicatorLabel->setVisible(false);
 }
 
 void PreciseOutputWindow::startEvaluation()
 {
     viewModel.setPrecisionAsync(ui->spinBox->value(), [this]() {emit evaluationFinished();});
     ui->evaluateButton->setText("Cancel");
-    // TODO: set the progress circle
+    ui->busyIndicatorLabel->setVisible(true);
 }
 
 void PreciseOutputWindow::interruptEvaluation()
